3345a3ed8ff2316b48b2e49c7956329684245333
Sarah <70871300+SarahAbdrabo@users.noreply.github.com>
2022-11-10 19:31:47 +0200

    main program that includes ISR

--- main.c
+++ main.c
@@ -14,13 +14,13 @@ includes of : DIO , GIE, EXT INT 0 + LI
 #include "EXT_I0_Private.h"
 
 
-
+/***************************************************************************************/
 
 void ISR_Sarah (void){
 
     if ( DIO_u8GetPinValue(PORTD, PIN2) == HIGH)
     {
-        DIO_voidSetPinValue(PORTC, PIN2, LOW);
+        DIO_VoidDigitalTogglePin(PORTC, PIN2);
     }
 
     }
@@ -30,36 +30,35 @@ void ISR_Sarah (void){
 int main(void)
 {
 
-    /**make all leds high, button connected on exti0, make led 1 low
+    	 /**make all leds high, button connected on exti0, make led 1 low
     LED2 D3
     LED1 C7
     LED0 C2
     INTPT PIN on pushbutton D2**/
 
 
-	DIO_voidSetPinDir(PORTC, PIN7, OUTPUT);  //DDR REG
+    DIO_voidSetPinDir(PORTC, PIN7, OUTPUT);  //DDR REG
     DIO_voidSetPinDir(PORTC, PIN2, OUTPUT);  //DDR REG
     DIO_voidSetPinDir(PORTD, PIN3, OUTPUT);  //DDR REG
-
-    DIO_voidSetPinDir(PORTD, PIN2, INPUT);  //DDR REG
+	/* the ext int 0 pin needs to be set as input but not pulled up. */
+    DIO_voidSetPinDir(PORTD, PIN2, INPUT);   //DDR REG
 
     DIO_voidSetPinValue(PORTC, PIN7, HIGH);
     DIO_voidSetPinValue(PORTC, PIN2, HIGH);
     DIO_voidSetPinValue(PORTD, PIN3, HIGH);
-
-    //EXTI0_SetCallBack(ISR_Sarah);
+	/*********************************Interrupt init. ***********************************/
+	
+    EXTI0_SetCallBack(ISR_Sarah);
 
 
     GIE_voidEnable();
     EXTI0_voidDInit();
-
     EXTI0_voidEnable();
 
-	/*Set all leds as HIGH, turn led off if i press the switch crosponding to it*/
-
+	
     while(1)
     {
-        /*call the isr*/
+        /*just call the isr and wait*/
 
     }
 
